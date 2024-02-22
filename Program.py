89cb3c3e59c44fd9265f4c91b581cfcf2ddd7f4d
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2024-02-22 12:35:33 +0000

    Setup
    building the basic code structure and importing pygame

---
+++ Program.py
@@ -0,0 +1,59 @@
+import pygame
+import math
+import random
+# Initialize the game engine
+pygame.init()
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+YELLOW = (255 , 255, 0)
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Project: ")
+
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+x_val = 0
+y_val = 0
+x_offset = 0
+y_offset = 0
+pi= 3.141592652
+counter = 0
+
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done = True # Flag that we are done so we exit this loop
+ 
+    # --- Game logic should go here
+ 
+    # --- Drawing code should go here
+ 
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+   
+        
+    screen.fill(WHITE)
+    
+    
+    
+    #draw stuff here:
+
+    
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+#endwhile
+pygame.quit()
