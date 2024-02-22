b38b7f094bd1f7679964096bcf21db3bbd390ba6
WillO1987 <81316260+WillO1987@users.noreply.github.com>
2024-02-22 12:44:35 +0000

    Addition of player class
    Player class with the ability to interact with objects and move and have future interactions with new classes created in future. e.g. walls. Added an Update method to the player and added the creation of sprite groups for the current objects.

--- Program.py
+++ Program.py
@@ -12,6 +12,8 @@ RED      = ( 255,   0,   0)
 BLUE     = (   0,   0, 255)
 YELLOW = (255 , 255, 0)
 size = (700, 500)
+moveSide = 0
+moveUp = 0
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Project: ")
 
@@ -20,8 +22,8 @@ done = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-x_val = 0
-y_val = 0
+x_val = 100
+y_val = 200
 x_offset = 0
 y_offset = 0
 pi= 3.141592652
@@ -41,8 +43,68 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 
+#endclass
+#adding a Player class that can interact with the Block objects 
+class Player(pygame.sprite.Sprite):
+    carry_Item_List = []
+
+    def __init__(self, s_width, s_length, initial_x, initial_y):
+        super().__init__()
+        self.x_val2 = x_val
+        self.width = s_width
+        self.height = s_length
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(GREEN), 
+        self.rect = self.image.get_rect()
+        self.rect.x = initial_x
+        self.rect.y = initial_y
+        self.diff_x = 0
+        self.diff_y = 0
+
+        
+    def update(self):
+         # Calculate diff_x and diff_y based on key input
+        keys = pygame.key.get_pressed()
+        self.diff_x = 0
+        self.diff_y = 0
+        if keys[pygame.K_LEFT]:
+            self.diff_x -= 2
+        if keys[pygame.K_RIGHT]:
+            self.diff_x += 2
+        if keys[pygame.K_UP]:
+            self.diff_y -= 2
+        if keys[pygame.K_DOWN]:
+            self.diff_y += 2
+
+        # Update the positions of the player and carried items
+        self.rect.x += self.diff_x
+        self.rect.y += self.diff_y
+
+        for item in self.carry_Item_List:
+            item.rect.x += self.diff_x
+            item.rect.y += self.diff_y
+        
+        # Check for collisions with walls
+        wall_collisions = pygame.sprite.spritecollide(self, wall_list, False)
+        for wall in wall_collisions:
+            if moveSide > 0:
+                self.rect.right = wall.rect.left
+            elif moveSide < 0:
+                self.rect.left = wall.rect.right
+            if moveUp > 0:
+                self.rect.bottom = wall.rect.top
+            elif moveUp < 0:
+                self.rect.top = wall.rect.bottom
 
  
+all_sprites = pygame.sprite.Group()
+wall_list = pygame.sprite.Group()
+item_sprite = pygame.sprite.Group()
+player_sprite = pygame.sprite.Group()
+
+player = Player(10, 10 , x_val , y_val )
+player_sprite.add(player)
+all_sprites.add(player)
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
